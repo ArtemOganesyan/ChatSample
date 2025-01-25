7c95ddd54bb2c56e2dffc26cb005f42e834ab482
Lorax55 <vladyslav.usa@gmail.com>
2025-01-24 18:09:25 -0800

    Added Automatic Saving (Version 1)

---
+++ src/java/SaveLog.java
@@ -0,0 +1,20 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+    public class SaveLog{
+        private static final String SAVE_FILE = "chat.log";
+        private static final String SAVE_SUCCESS = "Chat log saved to " + SAVE_FILE;
+        private static final String SAVE_ERROR = "Error saving chat log";
+
+        public void saveChatLog(String log) {
+            try (FileWriter writer = new FileWriter(SAVE_FILE, true)) {
+                writer.write(log + System.lineSeparator());
+                System.out.println(SAVE_SUCCESS);
+            } catch (IOException e) {
+                System.out.println(SAVE_ERROR);
+                e.printStackTrace();
+            }
+        }
+    }
+
